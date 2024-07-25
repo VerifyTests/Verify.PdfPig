6aff491c1a9321df5ca8cbb7f40474db392f6729
Simon Cropp <simon.cropp@gmail.com>
2024-07-26 07:36:16 +1000

    refs

---
+++ src/Verify.PdfPig/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using System.Diagnostics.CodeAnalysis;
+global using UglyToad.PdfPig;
+global using UglyToad.PdfPig.Content;
+global using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
